606eabe256bb05703dc75ba91f46ed1f76e0e883
Ben Pu <bpu991@gmail.com>
2020-12-14 19:01:35 -0500

    finished first two problems

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,82 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+//clarifying questions:
+    //1) can i assume that i will always get a string or
+    // could i also get an array of strings
+    //2) will the string only contain letters, or could it also contain
+    // numbers/ punctuation etc
+    //3) will the string always have an even number of characters?
 
+//approaches:
+    //1) split the string in half, if the reversed second half === first
+    // half, return true
+
+//pseudocode 1:
+    //1) determine if the string has an even amount of characters in it
+    //2) if it does:
+        // -split the string in half
+        // -reverse the second string
+        // -if str1 and str2 are equal, return true
+    //2) if it doesnt:
+        // -str1 === the string before the middle character
+        // -str2 === string after the middle character
+        // reverse str2
+        // -if str1 and str2 are equal, return true
+
+function palindrome(str) {
+    //1) determine if the string has an even amount of characters in it
+
+    //2) if it does:
+    if (str.length % 2 === 0) {
+         // -split the string in half
+        let middle = str.length / 2
+        let str1 = str.slice(0, middle)
+        let str2 = str.slice(middle)
+
+        // -reverse the second string
+        let reversedArr = str2.split('').reverse()
+
+        // -if str1 and str2 are equal, return true
+        if (str1 === reversedArr.join('')) {
+            return true
+        }  
+        return false
+    }
+
+    //2) if it doesnt:
+    else {
+
+        let middle = str.length / 2
+
+        // -str1 === the string before the middle character
+        let str1 = str.slice(0, middle)
+
+        // -str2 === string after the middle character
+        let str2 = str.slice(middle+1)
+
+         // reverse str2
+        let reversedArr = str2.split('').reverse();
+
+        // -if str1 and str2 are equal, return true
+        if (str1 === reversedArr.join('')) {
+            return true
+        }
+        return false
+    }
+}
+
+//pseudocode 2:
+    //1) reverse entire string
+    //2) if the reversed string === original string, return true
+    
+function palindrome(str) {
+    //1) reverse entire string
+    const reversed = str.split('').reverse().join('')
+
+    //2) if the reversed string === original string, return true
+    return reversed === str
+}
+
+console.log(palindrome('poop'))
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,49 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-console.log('hi')
+
+//clarifying questions:
+    // Can i assume I will always get a string, or will i sometimes get an array of strings?
+//Approaches:
+    //1) set a empty string variable, reverse for loop the str and then return
+    // the not so empty string variable at the end
+    //2) turn str input to an array, use .reverse on array
+//PseudoCode 1:
+    //1) set empty string variable
+    //2) reverse for loop the input string
+    //3) inside the for loop, add each element to the empty string
+    //4) return empty string
+//PseudoCode 2:
+    //1) turn str into an array
+    //2) call .reverse() method on said array
+    //3) .join() array to turn it back into a string
+
+//solution 1
+function reverse(str) {
+    //1) set empty string variable
+    let reverseString = '';
+
+    //2) reverse for loop the input string
+    for (let i = str.length - 1; i >= 0; i--) {
+
+        //3) inside the for loop, add each element to the empty string
+        reverseString+= str[i]
+    }
+    //4) return empty string
+    return reverseString;
+}
+
+
+//solution 2
+function reverse(str) {
+    //1) turn str into an array
+    let arr = str.split('')
+
+    //2) call .reverse() method on said array
+    let reversed = arr.reverse()
+
+    //3) .join() array to turn it back into a string
+    return reversed.join('')
+}
+console.log(reverse('hello'))
 module.exports = reverse;

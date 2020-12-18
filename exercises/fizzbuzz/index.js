ad712ae41cdb4c0cdd0166aec143408c57197671
Ben Pu <bpu991@gmail.com>
2020-12-17 19:02:40 -0500

    solved fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,33 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+//clarifying questions:
+    //1) will the input always be an integer. any data type conversions?
 
+//approaches:
+    // 1) run a for loop until it hits the input, with 4 conditionals
+
+//pseudo:
+    // 1) set up a for loop i = 0; i <= n; i++
+        // 2) print fizz if divisible by 3
+        // 3) print buzz if divisible by 5
+        // 4) print fizzbuzz if divisible by both
+
+function fizzBuzz(n) {
+
+    for(let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        }
+        else if(i % 3 ===0 ) {
+            console.log('fizz');
+        } else if (i%5 === 0) {
+            console.log('buzz');
+        }  else {
+            console.log(i);
+        }
+    }
+}
+
+console.log(fizzBuzz(0))
 module.exports = fizzBuzz;

116f38e4d6661355a0d91c3d3442ec7b845fdce0
Ben Pu <bpu991@gmail.com>
2020-12-17 19:19:08 -0500

    finished array chunking problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,26 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//approaches:
+    // 1) set up newArr var
+    // 2) loop through the array by increments of the size input
+    // 3) slice the input array and push that to new array
+    // 4) return new array
 
+//pseudo:
+    // let newArr = []
+    // for (let i = 0; i < array.length; i += size)
+        // slice the input array (array.slice(i, i+size))
+        // push that sub array into the new array
+    // return newArr
+function chunk(array, size) {
+    let newArr = [];
+    for(let i = 0; i < array.length; i += size) {
+        let subArr = array.slice(i, i+size)
+        newArr.push(subArr);
+    }
+    return newArr;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3))
 module.exports = chunk;

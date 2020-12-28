d65a6b3f9baa8e46e3bd22eab4199dc1e4d39768
Ben Pu <bpu991@gmail.com>
2020-12-28 16:39:31 -0500

    finished capitalize problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//clarifying questions
+    // 1) is this case sensitive? no
 
+// approaches:
+    //1) make two objects, one for stringA and one for stringB, compare the two 
+
+//pseudo :
+    // 1) define two empty objects
+    // 2) loop through both strings and make an object with the key = element
+        // the value = how many times it occurs
+    // 3) compare the two objects
+function anagrams(stringA, stringB) {
+    let obj1 = {};
+    let obj2 = {};
+    let count = 0;
+    for (let key of stringA) {
+        if(!obj1[key.toLowerCase()]) {
+            obj1[key.toLowerCase()] = 1
+        } else {
+            obj1[key.toLowerCase()]++
+        }
+    }
+    for (let key of stringB) {
+        if (!obj2[key.toLowerCase()]) {
+            obj2[key.toLowerCase()] = 1
+        } else {
+            obj2[key.toLowerCase()]++
+        }
+    }
+    for (let key1 in obj1) {
+        for(let key2 in obj2) {
+            if (key1 === key2 && obj1[key1] === obj2[key2]) {
+                count++
+            }
+        }
+    }
+    return count === Object.keys(obj1).length
+    
+}
+
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales'))
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,33 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+//Clarifying Questions:
+    // - will I always recieve a string input?
+    // - will the string only contain characters of the alphabet or will there
+    //  be numbers too?
 
+// Sample I/Os:
+    // - 'what's up you dirty dog' -> 'What's Up You Dirty Dog'
+    // - 'will i ever get a job?' -> 'Will I Ever Get A Job?'
+
+//Approach:
+    // split the string into an array at ' '
+    // iterate through each element of the array and capitalize the first letter
+    // join into new empty string
+
+//Pseudo:
+    // use .split(' ') to split the string into an array at each space
+    // use a for loop to loop through each element of the array
+    // at each index of the array, capitalize first element
+    // .join the array into a new string
+function capitalize(str) {
+    let array = str.split(' ');
+    let newSentence = [];
+    for (let i = 0; i < array.length; i++) {
+        newSentence.push(array[i][0].toUpperCase() + array[i].slice(1));
+    }
+    return newSentence.join(' ')
+}
+
+console.log(capitalize('hello what is up?'))
 module.exports = capitalize;
